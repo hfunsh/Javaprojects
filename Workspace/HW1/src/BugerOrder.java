22dad22439c27edb69a83cec3d275f87bde83c81
HFunsho <hammidfunsho@gmail.com>
2015-09-19 14:29:04 -0400

    backup

---
+++ Workspace/HW1/src/BugerOrder.java
@@ -0,0 +1,36 @@
+//This application will take orders for Burgers, fries & drinks, calculating a discount for any single item with a quantity greater than three. It will then calc total + tax and change due if applicable
+
+import java.util.*;
+
+//Declare Constants for the prices of items, along with sales tax
+
+
+
+public class BugerOrder {
+
+	public static void main(String[] args) {
+		//create a scanner object
+		
+		
+		//prompt the user to enter their order
+		
+		
+		//calculate the discount qty is greater than three for any item
+		
+		
+		//calculate the total price
+		
+		//calculate the price plus tax 
+		
+		
+		//prompt the user for payment
+		
+		
+		//calculate change due (if applicable)
+		
+		
+		//display the output
+
+	}
+
+}
